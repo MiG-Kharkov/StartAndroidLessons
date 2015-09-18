7bfe571abf36ef272eb025746b05e08e160e9576
Igor Manzhos <manzhos.igor@gmail.com>
2015-09-18 17:10:49 +0300

    Change Visible state menu, lesson 14

--- app/src/main/java/ua/com/webtuning/logandmess/MainActivity.java
+++ app/src/main/java/ua/com/webtuning/logandmess/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView tvOut;
     Button btnOk;
     Button btnCancel;
+    CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvOut = (TextView) findViewById(R.id.tvOut);
         btnOk = (Button) findViewById(R.id.btnOK);
         btnCancel = (Button) findViewById(R.id.btnCancel);
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
 
         btnOk.setOnClickListener(this);
         btnCancel.setOnClickListener(this);
@@ -35,6 +38,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    /**
+     * Prepare the Screen's standard options menu to be displayed.  This is
+     * called right before the menu is shown, every time it is shown.  You can
+     * use this method to efficiently enable/disable items or otherwise
+     * dynamically modify the contents.
+     * <p>
+     * <p>The default implementation updates the system menu items based on the
+     * activity's state.  Deriving classes should always call through to the
+     * base class implementation.
+     *
+     * @param menu The options menu as last shown or first initialized by
+     *             onCreateOptionsMenu().
+     * @return You must return true for the menu to be displayed;
+     * if you return false it will not be shown.
+     * @see #onCreateOptionsMenu
+     */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        boolean isChecked = checkBox.isChecked();
+        //Найти пункт меню по id ресурса
+        // getItem ищет по номеру порядковуму в списке размером в size()
+        menu.findItem(R.id.list1).setVisible(isChecked);
+
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
